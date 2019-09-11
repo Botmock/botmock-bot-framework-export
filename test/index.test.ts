6bb40731684450cda1a9fe79f6fbda09695943cf
Kevin Donahue <nonnontrivial@gmail.com>
2019-09-11 14:44:38 -0400

    add simple tests for remaining fields

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,11 +1,11 @@
-import path from "path";
+import { join } from "path";
 import { remove, mkdirp, readJson } from "fs-extra";
 import { writeToOutput } from "../";
 
-const outputDir = path.join(__dirname, "output");
-
-const PROJECT_NAME = "project";
 const INTENT_NAME = "intent"
+const PROJECT_NAME = "project";
+const outputDir = join(__dirname, "output");
+const filename = join(outputDir, `${PROJECT_NAME}.json`)
 
 const project = {
   id: "",
@@ -23,6 +23,7 @@ const project = {
     timezone: "UTC"
   },
 };
+
 const intents = [{
   id: "",
   name: INTENT_NAME,
@@ -39,6 +40,7 @@ const intents = [{
   },
   is_global: false
 }];
+
 const entities = [];
 
 // recreate output directory before each test runs
@@ -48,17 +50,29 @@ beforeEach(async () => {
   await writeToOutput({ project, intents, entities }, outputDir);
 });
 
+afterAll(async () => {
+  await remove(outputDir);
+});
+
 test("project data ends up in written json", async () => {  
-  const { name } = await readJson(path.join(outputDir, `${project.name}.json`));
+  const { name } = await readJson(filename);
   expect(name).toBe(PROJECT_NAME);
 });
 
 test("intent data ends up in written json", async () => {
-  const { intents } = await readJson(path.join(outputDir, `${project.name}.json`));
+  const { intents } = await readJson(filename);
   expect(intents).toHaveLength(1);
   expect(intents[0].name).toBe(INTENT_NAME);
 });
 
-test.todo("entity data ends up in written json");
-test.todo("pre-built entity data ends up in written json");
+test("utterance data ends up in written json", async () => {
+  const { utterances } = await readJson(filename);
+  expect(utterances).toHaveLength(0);
+});
+
+test("entity data ends up in written json", async () => {
+  const { entities } = await readJson(filename);
+  expect(entities).toHaveLength(0);
+});
+
 test.todo("fetches project assets");
