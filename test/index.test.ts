f56082f7dc47f3b991a213cf13b59fffa6a1124a
Kevin Donahue <nonnontrivial@gmail.com>
2019-09-17 10:02:25 -0400

    rebase

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,4 +2,17 @@ import { join } from "path";
 import { remove, readFile } from "fs-extra";
 import { default as FileWriter, restoreOutput } from "../lib/file";
 
-test.todo("");
+const outputDir = join(__dirname, "output");
+
+beforeEach(async () => {
+  await restoreOutput(outputDir);
+});
+
+afterAll(async () => {
+  await remove(outputDir);
+});
+
+test("write lg method creates file in output", async () => {
+  await new FileWriter({ outputDir, projectData: { project: { name: "project" }, intents: [] } }).writeLG();
+  expect((await readFile(join(outputDir, "project.lg"))).toString().startsWith(">")).toBe(true);
+});
