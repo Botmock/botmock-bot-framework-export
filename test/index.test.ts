368825719c1a11debdc2afe24bee484bc1a55054
Kevin Donahue <nonnontrivial@gmail.com>
2019-09-17 09:53:08 -0400

    import default from file in test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,3 +1,5 @@
-// import { app } from "../";
+import { join } from "path";
+import { remove, readFile } from "fs-extra";
+import { default as FileWriter, restoreOutput } from "../lib/file";
 
 test.todo("");
